cfba346809698bb08b45c3d345181444f8f841a2
richfjiq <rfjiq1986@gmail.com>
2023-10-22 16:29:43 -0600

    chore: add props and docs

--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -4,12 +4,40 @@ import MyLabel from '../components/MyLabel';
 const meta = {
   title: 'UI/labels/MyLabel',
   component: MyLabel,
+  tags: ['autodocs'],
   parameters: {
     layout: 'centered',
+  },
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+    },
   },
 } satisfies Meta<typeof MyLabel>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  args: {
+    label: 'Basic label',
+  },
+};
+
+export const AllCaps: Story = {
+  args: {
+    label: 'All Caps label',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Secondary label',
+  },
+};
+
+export const CustomColor: Story = {
+  args: {
+    label: 'Custom Color label',
+  },
+};
