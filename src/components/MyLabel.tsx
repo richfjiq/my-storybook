0d8ea834fec34e1f2601b258a3c079f04e65c9f9
richfjiq <rfjiq1986@gmail.com>
2023-10-22 16:29:43 -0600

    chore: add props and docs

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,7 +1,18 @@
 import './MyLabel.css';
 
-const MyLabel = () => {
-  return <span className="h1">Hello World</span>;
+interface Props {
+  /**
+   * Text to display
+   */
+  label: string;
+  /**
+   * Label size
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3';
+}
+
+const MyLabel = ({ label, size = 'normal' }: Props) => {
+  return <span className={`${size}`}>{label}</span>;
 };
 
 export default MyLabel;
